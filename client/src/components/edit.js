f4956028a4af410b37facbca68f3338d30344d54
Karl <karl.lu@outlook.com>
2023-01-31 16:45:22 -0500

    extracted form into its own component to reduce redundancy

--- client/src/components/edit.js
+++ client/src/components/edit.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router";
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import UserRecordForm from './userRecordForm';
 
 export default function Edit() {
     const [form, setForm] = useState({
@@ -64,50 +63,10 @@ export default function Edit() {
     };
 
     return (
-        <Form onSubmit={handleSubmit} className="col-10 col-lg-4 container-fluid justify-content-center card my-5 p-3">
-            <h3 className="text-center pt-3">Create New Record</h3>
-            <Form.Group
-                className="mb-3"
-                controlId="name">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="Name"
-                    value={form.name}
-                    onChange={(e) => updateForm({ name: e.target.value })}
-                />
-            </Form.Group>
-            <Form.Group
-                className="mb-3"
-                controlId="formPosition">
-                <Form.Label>Position</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="Position"
-                    value={form.position}
-                    onChange={(e) => updateForm({ position: e.target.value })}
-                />
-            </Form.Group>
-            <Form.Group
-                className="py-3 d-flex justify-content-around" controlId="formLevel"
-            >
-                {["Intern", "Junior", "Senior"].map((level) => (
-                    <Form.Check
-                        key={level}
-                        inline
-                        type="radio"
-                        label={level}
-                        name="positionOptions"
-                        id={`position${level}`}
-                        value={level}
-                        checked={form.level === level}
-                        onChange={(e) => updateForm({ level: e.target.value })}
-                    />
-                ))}
-            </Form.Group>
-            <Form.Group className="text-center py-3">
-                <Button type="submit">Update Record</Button>
-            </Form.Group>
-        </Form>
+        <UserRecordForm
+            form={form}
+            handleSubmit={handleSubmit}
+            updateForm={updateForm}
+        />
     )
 }
